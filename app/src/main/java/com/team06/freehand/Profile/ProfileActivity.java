3feaced83aa85efe1f2e8656bbf7a5ca79d3512e
isabellepotvin <isabellepotvin@yahoo.ca>
2018-02-28 10:47:40 -0500

    Started coding profile layout

--- app/src/main/java/com/team06/freehand/Profile/ProfileActivity.java
+++ app/src/main/java/com/team06/freehand/Profile/ProfileActivity.java
@@ -22,7 +22,7 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_explore);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: starting.");
 
         setupBottomNavigationView();
